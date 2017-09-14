b6bb37733a0b3dc181adfb6b2613222f67014344
mandarinx <mandarin@mandarin.no>
2017-09-14 09:16:31 +0200

    Added a file upload end point

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
-var express = require('express'),
-    bodyparser = require('body-parser'),
-    env = require('dotenv');
+var express     = require('express'),
+    fileupload  = require('express-fileupload'),
+    bodyparser  = require('body-parser'),
+    env         = require('dotenv');
 
 env.config();
 
 var app = express()
     .set('port', process.env.PORT)
-    .use(bodyparser.json());
+    .use(bodyparser.json())
+    .use(fileupload());
 
 if (process.env.NODE_ENV === 'development') {
     app.use('showStackError', true);
 }
 
 app.route('/buildcallback')
-    .post(function(req, res) {
+    .post(function (req, res) {
 
         console.log(req.body);
         res
             .status(200)
             .json({'oki':'doki'});
+
+    });
+
+app.route('/upload')
+    .post(function (req, res) {
+
+        if (!req.files) {
+            return res.status(400).send('No files were uploaded');
+        }
+
+        Object.keys(req.files).forEach(function(key) {
+            console.log('file: '+req.files[key].name);
+        });
+
+        res.status(200).send('OK');
 
     });
 
